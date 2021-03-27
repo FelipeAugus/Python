fc6c4a593d9555772535a7357f7792d2571ca66f
Felipe Augusto <57400329+FelipeAugus@users.noreply.github.com>
2021-03-26 21:52:58 -0300

    Update Matriz para triangular.py

--- Metodos Numéricos/Matriz para triangular.py
+++ Metodos Numéricos/Matriz para triangular.py
@@ -4,22 +4,31 @@ def setPivo(mat, L):
             return [L, c]
 
 def triang(mat):
-    """
-    Converte uma matriz quadrada qualquer para triangular inferior
-    """
+    # Converte uma matriz quadrada qualquer para triangular inferior
     for L in range(len(mat)):   
-        pivo = setPivo(mat, L)
-        
+        pivo = setPivo(mat, L) 
         #if (L!=len(mat)-1):
         for l in range(L+1, len(mat)):
-            det = (mat[l][pivo[1]] / mat[pivo[0]][pivo[1]])
-            
+            det = (mat[l][pivo[1]] / mat[pivo[0]][pivo[1]])     
             for c in range(len(mat)): 
                 mat[l][c] -= mat[pivo[0]][c]*det 
            
     return mat
 
-M = [[1,2,3],[4,5,6],[7,8,10]]
 
-N = [[0, 324, 123], [0, 0, 57], [31, 45, 97]]
-print (triang(N))
+ij = int(input('Tamanho da matriz: '))
+Mat = []
+
+for i in range(ij):
+    Mat.append([])
+    for j in range(ij):
+        Mat[i].append(int(input('Valor na posição '+str(i+1)+' '+str(j+1)+': ')))
+
+print('Matriz:')
+for i in range(ij):
+    print(Mat[i])
+
+print('\nTriangular: ')
+triang(Mat)
+for i in range(ij):
+    print(Mat[i])
